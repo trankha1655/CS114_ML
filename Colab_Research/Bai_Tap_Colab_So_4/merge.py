ac11169e5286e6a27e9be5f1c9066b23f7b65c00
trankha1655 <phamtruong7a1@gmail.com>
2021-06-11 17:54:22 +0700

    update colab_4

--- Colab_Research/Bai_Tap_Colab_So_4/merge.py
+++ Colab_Research/Bai_Tap_Colab_So_4/merge.py
@@ -1,17 +1,20 @@
 import pandas as pd 
 
+#read all file csv 
 
+#from Nghia
 df_nghia1= pd.read_csv('Crawl_by_Nghia\euronews.csv',index_col=0)
 df_nghia2= pd.read_csv('Crawl_by_Nghia\newyorker.csv')
 
+#from Kha
 df_kha1= pd.read_csv('Crawl_by_Kha\beaverton.csv',index_col=0)
 df_kha2= pd.read_csv('Crawl_by_Kha\babylon.csv',index_col=0)
 df_kha3= pd.read_csv('Crawl_by_Kha\citynews.csv',index_col=0)
 
+#from Lam
 df_lam = pd.read_csv('Crawl_by_Lam\TheTrueNorthTime.csv',index_col=0)
 
+#merge and save
 df= pd.DataFrame()
 df = df_kha2.append((df_kha3,df_kha1,df_nghia1,df_nghia2,df_lam))
-
-
-df.to_csv('data_crawl.csv',index=False)
+df.to_csv('dataset-news-is_sarcastic.csv',index=False)
